88326ff12c7fbef48f1391e6fdd9eed509c0cf84
Alex/Eris Norton <psyan15@nottingham.ac.uk>
2026-07-03 18:45:55 +0100

    Finished Splitter Function

--- lexer.c
+++ lexer.c
@@ -5,19 +5,20 @@
 
 #define BUFFER_LEN 256
 
-Queue* lexer(char* sourceCode){
+Queue* splitter(char* text){
     
     // Creating the list of words.
 
     Queue* words = initialise_queue();
     char* word;
-    int i,j = 0; // i refers to the word
-    while(sourceCode[i] != '\0'){
-        word = (char*) malloc(BUFFER_LEN);
-        j = 0; // j refers to the position in the word.
+    int i=0; // i refers to the word
+    int j=0;  // j refers to the position in the word.
 
-        while(sourceCode[i] != ' ' && sourceCode[i] != '\n' && sourceCode[i] != '\0'){
-            word[j] = sourceCode[i];
+    while(text[i] != '\0'){
+        word = (char*) malloc(BUFFER_LEN);
+        j = 0;
+        while(text[i] != ' ' && text[i] != '\n' && text[i] != '\0'){
+            word[j] = text[i];
             i++;
             j++;
         }
@@ -30,9 +31,20 @@ Queue* lexer(char* sourceCode){
         free(word);
     }
 
-    print_queue(words,print_string);
+    return words;
+}
+
+Queue* lexer(char* sourceCode){
+    Queue* words = splitter(sourceCode);
+    Queue* tokens = tokeniser(words);
 }
 
 int main(int argc, char** argv){
-    lexer("Hello World!\0");
+    char* source = (char*) malloc(BUFFER_LEN);
+    printf("Enter Source Code: ");
+    scanf("%[^\n]s",source); // ooh scanset specifier - this will only stop when it hits a newline.
+    
+    Queue* words = lexer(source);
+
+    free(source);
 }
