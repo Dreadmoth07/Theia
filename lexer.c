fcc0c5e8cbc3988ebaf7fba1cfc6b08ec112dd98
Alex/Eris Norton <psyan15@nottingham.ac.uk>
2026-07-03 21:37:41 +0100

    Added checking for bools, ints and variables.

--- lexer.c
+++ lexer.c
@@ -7,6 +7,7 @@
 #define BUFFER_LEN 256
 
 typedef enum {
+    TOKEN_NULL,
     // Operators
     TOKEN_ADD,
     TOKEN_SUB,
@@ -21,14 +22,16 @@ typedef enum {
     TOKEN_IN,
     // Types
     TOKEN_INT,
-    TOKEN_STRING,
+    TOKEN_CHAR,
+    TOKEN_BOOL,
     // Variable
     TOKEN_VARIABLE
 } Token;
 
 typedef struct {
     Token token;
-
+    char name[BUFFER_LEN];
+    void* value;
 } Lex;
 
 Queue* splitter(char* text){
@@ -62,55 +65,82 @@ Queue* splitter(char* text){
 
 Queue* tokeniser(Queue* words){
     Lex* tok = (Lex*) malloc(sizeof(Lex));
-    // check if a word is a known operator
-    char* word = (char*)pop_queue(words);
-    printf("%s",word);
-    switch (*word)
-    {
-        case '+':
-            tok->token = TOKEN_ADD;    
-            break;
-        case '-':
-            tok->token=TOKEN_SUB;
-            break;
-        case '*':
-            tok->token=TOKEN_MUL;
-            break;
-        case '/':
-            tok->token=TOKEN_DIV;
-            break;
-        case '%':
-            tok->token=TOKEN_MOD;
-            break;
-        case '^':
-            tok->token=TOKEN_EXP;
-            break;
-        case ',':
-            tok->token=TOKEN_CON;
-            break;
-        case '|':
-            tok->token=TOKEN_LEN;
-            break;
-        case '=':
-            tok->token=TOKEN_STO;
-            break;
-        case ':':
-            tok->token=TOKEN_OUT;
-            break;
-        case '~':
-            tok->token=TOKEN_IN;
-            break;
+    while(peek_queue(words)){
+        strcpy(tok->name,"");
+        tok->token=TOKEN_NULL;
+
+        // check if a word is a known operator
+        char* word = (char*)pop_queue(words);
+        printf("%s",word);
+        switch (*word)
+        {
+            case '+':
+                tok->token = TOKEN_ADD;    
+                break;
+            case '-':
+                tok->token=TOKEN_SUB;
+                break;
+            case '*':
+                tok->token=TOKEN_MUL;
+                break;
+            case '/':
+                tok->token=TOKEN_DIV;
+                break;
+            case '%':
+                tok->token=TOKEN_MOD;
+                break;
+            case '^':
+                tok->token=TOKEN_EXP;
+                break;
+            case ',':
+                tok->token=TOKEN_CON;
+                break;
+            case '|':
+                tok->token=TOKEN_LEN;
+                break;
+            case '=':
+                tok->token=TOKEN_STO;
+                break;
+            case ':':
+                tok->token=TOKEN_OUT;
+                break;
+            case '~':
+                tok->token=TOKEN_IN;
+                break;
+        }
+        
+        // check if a word is a known data type (int, bool, string)
+        // Since it is not necessarily a single-character word, we need to do string comparisons instead now.
+
+        // Integers
+        char* p=word;
+        int v = strtol(word,&p,10);
+        if(p != word){
+            tok->token = TOKEN_INT;
+            tok->value = &v;
+        }
+        // Booleans
+        if(strcmp(word,"true")==0){
+            tok->token=TOKEN_BOOL;
+            p=malloc(sizeof(int));
+            *p=1;
+            tok->value=p;
+        }
+        else if(strcmp(word,"false")){
+            tok->token=TOKEN_BOOL;
+            p=malloc(sizeof(int));
+            *p=0;
+            tok->value=p;
+        }
+
+        // if neither is true, it must be a variable of some kind.
+        if(tok->token == TOKEN_NULL){
+            strcpy(tok->name,word);
+            tok->token = TOKEN_VARIABLE;
+        }
+
+        free(word);
     }
-    
-
-    // check if a word is a known data type (int, bool, string)
-    // Since it is not necessarily a single-character word, we need to do string comparisons instead now.
-
-
-
-    // if neither is true, it must be a variable of some kind.
-
-    free(word);
 }
 
 Queue* lexer(char* sourceCode){
